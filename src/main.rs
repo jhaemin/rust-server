e55cb781118c0b8009497c1e5d2e599294811b13
Jang Haemin <io@jhaemin.com>
2021-10-23 23:57:32 +0900

    Init the project

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+use rocket::tokio::time::{sleep, Duration};
+
+#[macro_use]
+extern crate rocket;
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
+#[get("/delay/<seconds>")]
+async fn delay(seconds: u64) -> String {
+    sleep(Duration::from_secs(seconds)).await;
+    format!("Waited for {} seconds.", seconds)
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![index, world, delay])
+}
