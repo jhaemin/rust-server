c5598cf516d34e250fe3cc8a44a0f590d379b6dc
Jang Haemin <io@jhaemin.com>
2021-10-31 19:14:32 +0900

    Play with Rust

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::path::PathBuf;
+
 use rocket::tokio::time::{sleep, Duration};
 
 #[macro_use]
@@ -14,7 +16,54 @@ async fn delay(seconds: u64) -> String {
     format!("Waited for {} seconds.", seconds)
 }
 
-#[launch]
-fn rocket() -> _ {
-    rocket::build().mount("/", routes![index, delay])
+#[get("/hello/<name>/<age>/<cool>")]
+fn hello(name: &str, age: u8, cool: bool) -> String {
+    if cool {
+        format!("You're a cool {} year old, {}!", age, name)
+    } else {
+        format!("{}, we need to talk about your coolness.", name)
+    }
+}
+
+#[get("/borrow")]
+fn borrow() -> String {
+    fn change(some_string: &mut String) {
+        some_string.push_str(", world!");
+    }
+
+    fn first_word(s: &String) -> usize {
+        let bytes = s.as_bytes();
+
+        for (i, &item) in bytes.iter().enumerate() {
+            if item == b' ' {
+                return i;
+            }
+        }
+
+        s.len()
+    }
+
+    let mut s = String::from("hello world");
+
+    let word = first_word(&s);
+
+    s.clear();
+
+    let s1 = String::from("hello");
+    let mut s2 = s1; // s1 moved to s2 then s1 is no longer valid after this line
+
+    change(&mut s2);
+
+    format!("{}, world!", s2)
+}
+
+#[get("/page/<path..>")]
+fn get_page(path: PathBuf) -> String {
+    format!("{}", path.as_path().display().to_string())
+}
+
+#[rocket::main]
+#[allow(unused_must_use)]
+async fn main() {
+    rocket::build().mount("/", routes![index, delay, hello, get_page, borrow]).launch().await;
 }
